80e271015f19b6f722bad56189a7fff6063ba7f9
GeekJulia <juliayaks148@gmail.com>
2025-04-02 00:06:46 +0100

    Just testing out express

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+const express = require('express') // require the express
+const app = express() // we create an application to help us set up our entire server
+
+//to make it actuall run
+//the application is listening on port 3000 for a bunch of request
+app.get("/",(req,res) =>{ //anytime they navigate to /
+    console.log("Here")
+    res.render("index")
+})
+app.listen(3000)
